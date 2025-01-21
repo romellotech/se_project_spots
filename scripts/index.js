477d29a85b0f3a871394f12810b35bb31966797a
clarence <chymanjr@gmail.com>
2025-01-20 19:13:02 -0500

    added validation.js and updated java and modal.css

--- scripts/index.js
+++ scripts/index.js
@@ -40,6 +40,7 @@ const editModalDescriptionInput = editModal.querySelector(
 
 const cardModal = document.querySelector("#add-card-modal");
 const cardForm = cardModal.querySelector(".modal__form");
+const cardSubmitBtn = cardModal.querySelector("modal__button");
 const cardModalCloseBtn = cardModal.querySelector(".modal__close-btn");
 const cardNameInput = cardModal.querySelector("#add-card-name-input");
 const cardLinkInput = cardModal.querySelector("#add-card-link-input");
@@ -105,6 +106,7 @@ function handleAddCardSubmit(evt) {
   cardsList.prepend(cardElement);
   closeModal(cardModal);
   evt.target.reset();
+  disableButton(cardSubmitBtn);
 }
 
 profileEditButton.addEventListener("click", () => {
